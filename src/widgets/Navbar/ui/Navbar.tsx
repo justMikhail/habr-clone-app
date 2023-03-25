eccd17d9473502c67d5c146672cd98209e2360d3
justMikhail <followupmikhail@gmail.com>
2023-03-25 22:05:59 +0300

    fix/fix translation

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,27 +1,32 @@
 import classNames from 'classnames';
 import { SharedLink, SharedLinkTheme } from 'shared/ui/SharedLink/SharedLink';
+import { useTranslation } from 'react-i18next';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-  <div className={classNames(styles.Navbar, className)}>
-    <div className={styles.linksList}>
-      <SharedLink
-        to="/"
-        theme={SharedLinkTheme.SECONDARY}
-      >
-        Home
-      </SharedLink>
+export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
 
-      <SharedLink
-        to="/about"
-        theme={SharedLinkTheme.SECONDARY}
-      >
-        About
-      </SharedLink>
+  return (
+    <div className={classNames(styles.Navbar, className)}>
+      <div className={styles.linksList}>
+        <SharedLink
+          to="/"
+          theme={SharedLinkTheme.SECONDARY}
+        >
+          {t('Домашняя')}
+        </SharedLink>
+
+        <SharedLink
+          to="/about"
+          theme={SharedLinkTheme.SECONDARY}
+        >
+          {t('О сайте')}
+        </SharedLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
